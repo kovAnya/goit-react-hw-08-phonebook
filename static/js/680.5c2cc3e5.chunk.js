dd8a090108c0a49e52cd6459739534c17f3ac26e
kovAnya <104009473+kovAnya@users.noreply.github.com>
2023-01-08 23:58:48 +0000

    Deploying to gh-pages from @ kovAnya/goit-react-hw-08-phonebook@641889ce91dfe5460662326428f662fee6426140 🚀

---
+++ static/js/680.5c2cc3e5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[680],{680:function(e,a,r){r.r(a);var t=r(3329);a.default=function(){return(0,t.jsx)("div",{children:(0,t.jsx)("h1",{children:"Task manager welcome page "})})}}}]);
+//# sourceMappingURL=680.5c2cc3e5.chunk.js.map
