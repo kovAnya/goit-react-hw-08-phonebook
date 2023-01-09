45bcb20fd865d8c1d8645eeb1cc318e13a891953
kovAnya <104009473+kovAnya@users.noreply.github.com>
2023-01-09 05:59:55 +0000

    Deploying to gh-pages from @ kovAnya/goit-react-hw-08-phonebook@7216fea8165da5fdce257dd7fb0e185aefad5c01 🚀

---
+++ static/js/156.2e6546bb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[156],{156:function(n,e,t){t.r(e),t.d(e,{default:function(){return s}});var a,r,c=t(168),o=t(931),i=o.Z.div(a||(a=(0,c.Z)(["\n  margin-top: 90px;\n"]))),u=o.Z.h1(r||(r=(0,c.Z)(["\n  font-size: 20px;\n  font-weight: normal;\n  text-align: center;\n"]))),l=t(329),s=function(){return(0,l.jsx)(i,{children:(0,l.jsx)(u,{children:"Save and manage your contacts easily!"})})}}}]);
+//# sourceMappingURL=156.2e6546bb.chunk.js.map
