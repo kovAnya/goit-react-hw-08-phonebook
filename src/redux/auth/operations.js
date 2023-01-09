2ed26deb644c1379bada7d700b302008d2a41c58
Anna Kovalchuk <annazukh@gmail.com>
2023-01-09 03:46:31 +0200

    update

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -15,40 +15,42 @@ const clearAuthHeader = () => {
 
 export const signUp = createAsyncThunk(
   'auth/signup',
-  async (credentials, thunkApi) => {
+  async (credentials, thunkAPI) => {
     try {
       const response = await axios.post('/users/signup', credentials);
       setAuthHeader(response.data.token);
       return response.data;
     } catch (e) {
-      return thunkApi.rejectWithValue(e.message);
+      return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
 
 export const logIn = createAsyncThunk(
   'auth/login',
-  async (credentials, thunkApi) => {
+  async (credentials, thunkAPI) => {
     try {
       const response = await axios.post('/users/login', credentials);
       setAuthHeader(response.data.token);
       return response.data;
     } catch (e) {
-      return thunkApi.rejectWithValue(e.message);
+      return thunkAPI.rejectWithValue(e.message);
     }
   }
 );
 
-export const logOut = createAsyncThunk('auth/logout', async (_, thunkApi) => {
+export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
     await axios.post('/users/logout');
     // After a successful logout, remove the token from the HTTP header
     clearAuthHeader();
-  } catch (e) {}
+  } catch (e) {
+    return thunkAPI.rejectWithValue(e.message);
+  }
 });
 
 export const refreshUser = createAsyncThunk(
-  'auth/refreshuser',
+  'auth/refresh',
   async (_, thunkAPI) => {
     // Reading the token from the state via getState()
     const state = thunkAPI.getState();
--- src/redux/store.js
+++ src/redux/store.js
@@ -15,7 +15,7 @@ import {
 import storage from 'redux-persist/lib/storage';
 
 const persistConfig = {
-  key: 'root',
+  key: 'auth',
   storage,
   whitelist: ['token'],
 };
